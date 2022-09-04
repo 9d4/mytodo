9242605a0bf65344d186ac2572dd13a1bd7ac40f
Dimas Ananda traper <traperwaze@gmail.com>
2022-09-04 11:56:25 +0700

    fix: double error messages

--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -21,7 +20,6 @@ func init() {
 
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
 		os.Exit(1)
 	}
 }
